40c4f6c7eb02721148580e4af776d317b9a9fdf2
Yosef Malka <yosef.malka@gmail.com>
2020-12-15 19:23:36 +0200

    fix order->offer start working on the data context

--- CommunityLibrary/data_Access/App_Code/DataContext.cs
+++ CommunityLibrary/data_Access/App_Code/DataContext.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using data_Access.Models;
 
 namespace data_Access.App_Code
 {
     public class DataContext
     {
-
+        public DbSet<User> Users { get; set; }
+        public DbSet<Offer> 
     }
 }
--- CommunityLibrary/data_Access/Models/Order.cs
+++ CommunityLibrary/data_Access/Models/Order.cs
@@ -7,14 +7,7 @@ namespace data_Access.Models
 {
     public class Order
     {
-        public int Id { get; set; }
-        public Book Book { get; set; }
-        public String StatusDescription { get; set; }
-        public User OfferUser { get; set; }
-        public DateTime? CreateDate { get; set; }
-        public int DaysForGive { get; set; }
-        public int DaysForReturn { get; set; }
-        public List<Comment> Comments { get; set; }
+
 
     }
 }
