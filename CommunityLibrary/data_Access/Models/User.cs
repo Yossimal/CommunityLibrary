3fd518c03302736762cc2125ab9e2477f789ac4f
Meir Klemfner <meirk251@outlook.com>
2020-12-15 19:47:13 +0200

    Created User entity and Address entity.

--- CommunityLibrary/data_Access/Models/User.cs
+++ CommunityLibrary/data_Access/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,18 @@ namespace data_Access.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public int LendingCount { get; set; }
+        public Roles Role { get; set; }
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+
+        public enum Roles { Basic, Premium, Administrator }
     }
 }
