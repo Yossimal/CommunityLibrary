b80aa561dfead43c08dae6cdd78899b3709c4d40
Yosef Malka <yosef.malka@gmail.com>
2020-12-15 19:41:40 +0200

    finish basic data context

--- CommunityLibrary/data_Access/App_Code/DataContext.cs
+++ CommunityLibrary/data_Access/App_Code/DataContext.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.UI;
 using data_Access.Models;
 
 namespace data_Access.App_Code
 {
-    public class DataContext
+    public class DataContext:DbContext
     {
         public DbSet<User> Users { get; set; }
-        public DbSet<Offer> 
+        public DbSet<Offer> Offers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        public DataContext():base()
+        {
+        }
     }
 }
