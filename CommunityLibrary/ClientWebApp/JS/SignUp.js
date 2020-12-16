163f37bb22671fd93e29ccbe73e7283ba44c753a
Meir Klemfner <meirk251@outlook.com>
2020-12-16 18:29:14 +0200

    Updated SignUp fixed naming.

--- CommunityLibrary/ClientWebApp/JS/SignUp.js
+++ CommunityLibrary/ClientWebApp/JS/SignUp.js
@@ -7,25 +7,25 @@ $("#signUp").click(() => {
 
     let data = {
         userName: $("#userName").val(),
-        password1: $("#password1").val(),
+        password: $("#password1").val(),
         firstName: $("#firstName").val(),
         lastName: $("#lastName").val(),
         dayBirth: birthDate.getDate(),
         monthBirth: birthDate.getMonth() + 1,
         yearBirth: birthDate.getFullYear(),
-        email: $("#email").val(),
-        phone: $("#phoneNumber").val(),
-        apartmentNumber: $("#apartmentNumber").val(),
+        emailAddress: $("#email").val(),
+        phoneNumber: $("#phoneNumber").val(),
+        apartment: $("#apartmentNumber").val(),
         enterance: $("#enterance").val(),
         houseNumber: $("#houseNumber").val(),
         city: $("#city").val(),
         state: $("#state").val(),
-        zip: $("#zip").val(),
+        zipCode: $("#zip").val(),
         country: $("#country").val(),
-        street: $("#streetAddress").val(),
+        streetAddress: $("#streetAddress").val(),
     }
 
-    if (data.password1 != $("#password2").val()) return;
+    if (data.password != $("#password2").val()) return;
 
     let jsonData = JSON.stringify(data);
 
