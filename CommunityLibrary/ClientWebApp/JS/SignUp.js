e8b826f30c4efc6a83a0c040deed1d492b5a4cad
Meir Klemfner <meirk251@outlook.com>
2020-12-16 17:17:54 +0200

    Started SignUp.js

---
+++ CommunityLibrary/ClientWebApp/JS/SignUp.js
@@ -0,0 +1,12 @@
+﻿
+$("#signUp").click(() => {
+    data = $("form").serialize()
+
+    alert(data);
+    //POST(
+    //    "https://localhost:44333/HttpAPI/CommunityLibraryPrivate.asmx/AddUser",
+    //    data,
+    //    (data) => { alert('success: ' + data) },
+    //    () => { alert('error') }
+    //)
+})
