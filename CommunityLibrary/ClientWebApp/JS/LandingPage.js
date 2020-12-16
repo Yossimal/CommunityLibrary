93ebe2babd78154506222b67c2bc7719d53f2502
Meir Klemfner <meirk251@outlook.com>
2020-12-16 19:47:11 +0200

    Finished landing page.

--- CommunityLibrary/ClientWebApp/JS/LandingPage.js
+++ CommunityLibrary/ClientWebApp/JS/LandingPage.js
@@ -1,3 +1,23 @@
 ﻿$("#signUp").click(() => {
     $(location).attr('href', '/HTML/SignUp.html')
 });
+
+$("#discover").click(() => {
+    $(location).attr('href', '/HTML/BooksMap.html')
+});
+
+$("#signIn").click(() => {
+    let data = {
+        userName: $("#UserName").val(),
+        password: $("#Password").val(),
+    }
+
+    if (!data.userName || !data.password) return;
+
+    POST(
+        "https://localhost:44333/HttpAPI/CommunityLibraryPrivate.asmx/CheckLogin",
+        data,
+        (s) => { alert("success " + s) },
+        () => { alert("error signing in") }
+    )
+});
